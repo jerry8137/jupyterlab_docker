92e2f406ef8cf153533b038d80d4f6061aeb261d
Jerry <jerry8137@gmail.com>
2022-07-26 13:02:15 +0800

    add share memory arg

--- run_jupyter_docker.sh
+++ run_jupyter_docker.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --rm --gpus all -it -p 8848:8888 -p 2232:22 -v ~/jupyter_data:/home -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root  --name gpu-jupyter_1 softmac/jupyterlab:latest
+docker run --rm --gpus all -it -p 8848:8888 -p 2232:22 -v ~/jupyter_data:/home -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root  --name gpu-jupyter_1 --shm-size 60G softmac/jupyterlab:latest
